be30f7c64d90ae2a1f61c5e897769332f15ca9d9
GoOneTech <goonetechnologies@gmail.com>
2023-04-08 01:49:10 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App" style={{ marginTop: '30px' }}>
+    <div className="App" style={{ marginTop: '100px', border:'1px solid green' }}>
       Trial
     </div>
   );
