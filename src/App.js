effd23e56e0ddf12bf6421d4bde12633794b134f
GoOneTech <goonetechnologies@gmail.com>
2023-04-08 01:53:14 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App" style={{ marginTop: '100px', border:'1px solid green' }}>
+    <div className="App" style={{ marginTop: '100px', border: '1px solid green' }}>
       Trial
+      <BrowserRouter>
+        <Routes>
+          <Route path="/:message" element={<Home />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
