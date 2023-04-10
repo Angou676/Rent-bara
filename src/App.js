352495398fc119b82122a39cc4ac04941a7c1f4a
GoOneTech <goonetechnologies@gmail.com>
2023-04-10 14:42:30 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import Trial from './Trial';
 
 function App() {
   return (
-    <div className="App" style={{ marginTop: '100px', border: '3px solid blue' }}>
-      Trial
+    <div className="App" style={{ marginTop: '100px', border: '3px solid green' }}>
       <BrowserRouter>
         <Routes>
+          <Route path="/" exact element={<Trial />} />
           <Route path="/:id/:lat/:long" exact element={<Home />} />
 
         </Routes>
