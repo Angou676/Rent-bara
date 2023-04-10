925c38864b7951c7c558a61d77c546265d83f133
GoOneTech <goonetechnologies@gmail.com>
2023-04-10 14:56:00 +0530

    code add

--- src/ShowPage.js
+++ src/ShowPage.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const ShowPage = () => {
-  return (
-    <div>ShowPage here</div>
-  )
+    return (
+        <div>ShowPage here {useLocation().pathname}</div>
+    )
 }
 
 export default ShowPage
