267f0c823987bff6e97d76b0ab74b3225a187c76
GoOneTech <goonetechnologies@gmail.com>
2023-04-10 14:52:53 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Trial from './Trial';
+import ShowPage from './ShowPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Trial />} />
           <Route path="/:id/:lat/:long" exact element={<Home />} />
+          <Route path="/show" exact element={<ShowPage />} />
 
         </Routes>
       </BrowserRouter>
---
+++ src/ShowPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowPage = () => {
+  return (
+    <div>ShowPage here</div>
+  )
+}
+
+export default ShowPage
