e2084041767db4cd7002358a01ef2349f84d32cc
GoOneTech <goonetechnologies@gmail.com>
2023-04-10 14:19:00 +0530

    code added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import { useLocation } from 'react-router-dom'
 
 function App() {
-  const data = useLocation()
-  console.log(data)
   return (
     <div className="App" style={{ marginTop: '100px', border: '3px solid blue' }}>
       Trial
